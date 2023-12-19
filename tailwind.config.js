12edd86f6a58a1ffe9d29ff25f7e7a18dd588caf
alessandro dandea <alessandromanueldandea@gmail.com>
2023-12-19 15:51:39 +0100

    aggiunto navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./src/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        fontBig:['Bebas+Neue']
+      },
+      colors:{
+        main1:'#88E68B',
+        main2:'#F8F0FB',
+        main3:'#211A1D',
+        main4:'#6320EE',
+        main5:'#4690FF'
+      }
+    },
   },
   plugins: [],
 }
