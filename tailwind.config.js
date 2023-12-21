90481ea3c2fbda874d69b849a3067b9a43333aa0
alessandro dandea <alessandromanueldandea@gmail.com>
2023-12-21 10:17:16 +0100

    aggiunte slide

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
   content: ['./src/**/*.html'],
   theme: {
     extend: {
+      animation: {
+        left_entrance: 'left_entrance 1s ease-in-out',
+      },
+      keyframes: {
+        left_entrance: {
+          '0%': {opacity:'0'},
+          
+          '100%': {opacity:'1'},
+        }
+      },
+
+
+
+
       fontFamily:{
         fontBig:['Bebas+Neue']
       },
